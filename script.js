99b2bea4d35ab6bb02b8759229470eb7b5fd4cbc
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2024-07-17 17:02:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function validateForm() {
-  let form = document.forms["form"]["post"].value;
-  let submit = document.getElementById("submit");
+// function validateForm() {
+//   let form = document.forms["form"]["post"].value;
+//   let submit = document.getElementById("submit");
 
-  if (form == "") {
-    alert("OOps");
-    return false;
+//   if (form == "") {
+//     alert("OOps");
+//     return false;
+//   }
+// }
+
+let form = document.getElementById("form");
+let clickPost = document.getElementById("post");
+let submitButton = document.getElementById("submit");
+let errorMessage = "Oops! Please add your email";
+errorMessage.style.color = "red";
+
+function formValidate() {
+  if (clickPost.value == "") {
+    // alert("Oops! Please add your email")
+    // errorMessage.style.color = "red";
+    // form.innerHTML = errorMessage;
+    form.insertAdjacentHTML("afterend", errorMessage);
   }
 }
