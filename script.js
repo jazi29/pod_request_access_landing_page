647f838b3471a9e055e68c12082d4242e121a907
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2024-08-20 00:21:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
-// alert("Oops! Please add your email")
-// errorMessage.style.color = "red";
-// form.innerHTML = errorMessage;
+let clickPost = document.getElementById("post");
+// let email = document.getElementById("email");
+let errorMessage = document.getElementById("error-message");
+let checkMessage = document.getElementById("check-message");
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-// let form = document.getElementById("form");
-
-// function formValidate() {
-//   if (clickPost.value == "") {
-//     errorMessage.style.display = "block";
-//     event.preventDefault();
-
-//     clickPost.addEventListener("input", function (event) {
-//       // console.log("Пользователь вводит текст: " + event.target.value);
-//       errorMessage.style.display = "none";
-
-//       if (email.validity.typeMismatch) {
-//         // errorMessage.style.display = "block";
-//         alert("No");
-//         event.preventDefault();
-//       }
-//     });
-//   }
-// }
-
-document.getElementById("form").addEventListener("submit", function (event) {
-  let clickPost = document.getElementById("post");
-  let email = document.getElementById("email");
-  let errorMessage = document.getElementById("error-message");
-  let checkMessage = document.getElementById("check-message");
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+document.getElementById("form").addEventListener("click", function (event) {
+  event.preventDefault();
   if (clickPost.value === "") {
     errorMessage.style.display = "block";
     checkMessage.style.display = "none";
-    event.preventDefault();
-  } else {
-    errorMessage.style.display = "none";
+    console.log("It's work");
+    // event.preventDefault();
   }
-
-  clickPost.addEventListener("input", function () {
+  clickPost.addEventListener("input", function (event) {
     if (clickPost.value !== "") {
-      errorMessage.style.display = "none";
-
+      errorMessage.style.display = "block";
+      checkMessage.style.display = "none";
       console.log("Пользователь вводит текст: " + event.target.value);
     }
+
+    if (!isValidLogin(clickPost.value)) {
+      checkMessage.style.display = "block";
+      errorMessage.style.display = "none";
+      // event.preventDefault();
+    } else {
+      errorMessage.style.display = "none";
+      checkMessage.style.display = "none";
+      form.submit();
+      alert("All good");
+    }
+    function isValidLogin(clickPost) {
+      emailPattern.test(clickPost.value);
+    }
   });
-
-  if (!emailPattern.test(email)) {
-    checkMessage.style.display = "block";
-    errorMessage.style.display = "none";
-
-    event.preventDefault();
-  } else {
-    checkMessage.style.display = "none";
-  }
-
-  // if (email.validity.typeMismatch) {
-  //   alert("Пожалуйста, введите корректный email-адрес.");
-  //   event.preventDefault();
-  // }
 });
+
+// if (!emailPattern.test(email)) {
+//   errorMessage.style.display = "none";
+//   checkMessage.style.display = "block";
+//   // alert("O");
+
+//   event.preventDefault();
+// } else {
+//   checkMessage.style.display = "none";
+// }
+
+//   if (emailPattern.match(email)) {
+//     event.preventDefault();
+//     checkMessage.style.display = "block";
+//     errorMessage.style.display = "none";
+//     console.log("email is wrong");
+//   } else {
+//     checkMessage.style.display = "none";
+//     errorMessage.style.display = "block";
+//     console.log("email is right");
+//   }
+// });
