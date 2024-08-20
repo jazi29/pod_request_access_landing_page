08b8400962a32d1eabf7206c64cfff00f0eba46d
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2024-08-20 22:26:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,31 @@
+let form = document.getElementById("form");
 let clickPost = document.getElementById("post");
-// let email = document.getElementById("email");
 let errorMessage = document.getElementById("error-message");
 let checkMessage = document.getElementById("check-message");
 const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-document.getElementById("form").addEventListener("click", function (event) {
-  event.preventDefault();
+form.addEventListener("submit", function (event) {
   if (clickPost.value === "") {
     errorMessage.style.display = "block";
     checkMessage.style.display = "none";
-    console.log("It's work");
-    // event.preventDefault();
+    event.preventDefault();
+    return;
   }
-  clickPost.addEventListener("input", function (event) {
-    if (clickPost.value !== "") {
-      errorMessage.style.display = "block";
-      checkMessage.style.display = "none";
-      console.log("Пользователь вводит текст: " + event.target.value);
-    }
 
-    if (!isValidLogin(clickPost.value)) {
-      checkMessage.style.display = "block";
-      errorMessage.style.display = "none";
-      // event.preventDefault();
-    } else {
-      errorMessage.style.display = "none";
-      checkMessage.style.display = "none";
-      form.submit();
-      alert("All good");
-    }
-    function isValidLogin(clickPost) {
-      emailPattern.test(clickPost.value);
-    }
-  });
+  if (!emailPattern.test(clickPost.value)) {
+    checkMessage.style.display = "block";
+    errorMessage.style.display = "none";
+    event.preventDefault();
+    return;
+  }
+
+  errorMessage.style.display = "none";
+  checkMessage.style.display = "none";
+
+  // alert("All good");
 });
 
-// if (!emailPattern.test(email)) {
-//   errorMessage.style.display = "none";
-//   checkMessage.style.display = "block";
-//   // alert("O");
-
-//   event.preventDefault();
-// } else {
-//   checkMessage.style.display = "none";
-// }
-
-//   if (emailPattern.match(email)) {
-//     event.preventDefault();
-//     checkMessage.style.display = "block";
-//     errorMessage.style.display = "none";
-//     console.log("email is wrong");
-//   } else {
-//     checkMessage.style.display = "none";
-//     errorMessage.style.display = "block";
-//     console.log("email is right");
-//   }
-// });
+clickPost.addEventListener("input", function (event) {
+  errorMessage.style.display = "none";
+  checkMessage.style.display = "none";
+});
